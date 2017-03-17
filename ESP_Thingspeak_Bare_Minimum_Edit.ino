1b174c9930de4f5989f739e1cfe1a03f1454ae0a
Zish117 <shaunzim@live.ca>
2017-03-16 22:10:55 -0400

    Experiment
    Bare minimum sketch.

---
+++ ESP_Thingspeak_Bare_Minimum_Edit.ino
@@ -0,0 +1,38 @@
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+   
+char ssid[] = "YOURNETWORK";    //  your network SSID (name) 
+char pass[] = "YOURPASSWORD";   // your network password
+
+int status = WL_IDLE_STATUS;
+WiFiClient  client;
+
+// On ESP8266:  0 - 1023 maps to 0 - 1 volts
+#define VOLTAGE_MAX 1.0
+#define VOLTAGE_MAXCOUNTS 1023.0
+
+unsigned long myChannelNumber = 31461;
+const char * myWriteAPIKey = "LD79EOAAWRVYF04Y";
+
+void setup() 
+{
+  WiFi.begin(ssid, pass);
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading 
+  // On Uno,Mega,YunArduino:  0 - 1023 maps to 0 - 5 volts
+  // On ESP8266:  0 - 1023 maps to 0 - 1 volts
+  // On MKR1000,Due: 0 - 4095 maps to 0 - 3.3 volts
+  float voltage = sensorValue * (VOLTAGE_MAX / VOLTAGE_MAXCOUNTS);
+
+  int sample = 17;
+
+  // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+  // pieces of information in a channel.  Here, we write to field 1.
+  ThingSpeak.writeField(myChannelNumber, 1, sample, myWriteAPIKey);
+  delay(20000); // ThingSpeak will only accept updates every 15 seconds.
+}
